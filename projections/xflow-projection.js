dd98c0f7409a9933ad14cbf9db24a56c5a1308aa
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-03-23 12:11:15 +0100

    Added a zoom value to the orthographic operator

--- projections/xflow-projection.js
+++ projections/xflow-projection.js
@@ -1,7 +1,5 @@
 
-var Xflow = Xflow || {};
-var XML3D = XML3D || {};
-	
+
 (function() {
 
 Xflow.registerOperator("xflow.ortho", {
@@ -12,19 +10,20 @@ Xflow.registerOperator("xflow.ortho", {
     params:  [
         { type: 'float2', source: 'viewport' },
         { type: 'float', source: 'near' },
-        { type: 'float', source: 'far' }
+        { type: 'float', source: 'far' },
+		{ type: 'float', source: 'zoom'}
     ],
 	
-    alloc: function(sizes, viewport, near, far)
+    alloc: function(sizes, viewport, near, far, zoom)
     {
         sizes['perspective'] = 1;
     },
 	
-    evaluate: function(perspective, viewport, near, far)
+    evaluate: function(perspective, viewport, near, far, zoom)
 	{
 		var w = viewport[0], h = viewport[1];
-		var a = w / h;
-		var r = a, l = -a, t = 1.0, b = -1.0, f = far[0], n = near[0];
+		var a = (w / h) * zoom[0];
+		var r = a, l = -a, t = zoom[0], b = -zoom[0], f = far[0], n = near[0];
 
 		XML3D.math.mat4.ortho(perspective, l, r, b, t, n, f);
 		console.log(perspective);
